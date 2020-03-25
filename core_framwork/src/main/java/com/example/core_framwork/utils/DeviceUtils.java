606a2ac620ff94635e318edca410697a43a187c4
lfbxzlx <lfbxzlx@163.com>
2020-03-25 11:18:32 +0800

    添加获取应用名称的方法

--- core_framwork/src/main/java/com/example/core_framwork/utils/DeviceUtils.java
+++ core_framwork/src/main/java/com/example/core_framwork/utils/DeviceUtils.java
@@ -520,6 +520,20 @@ public class DeviceUtils {
         }
         return name;
     }
+// 获取应用名称
+    public static synchronized String getAppName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            int labelRes = packageInfo.applicationInfo.labelRes;
+            return context.getResources().getString(labelRes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     public static boolean isScreenOn(Context context) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
